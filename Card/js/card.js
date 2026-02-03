8e3bbf66835006dbb5aa93c6fbd1388e9c28cdee
Ahmed Mohamed <ahmedmohamedside1235@email.com>
2026-02-03 19:34:22 +0200

    change path

--- Card/js/card.js
+++ Card/js/card.js
@@ -45,7 +45,7 @@ function removeFromCart(index) {
 }
 
 function checkout() {
-  window.location.href = "../Card/cart.html";
+  window.location.href = "../Cart/card.html";
 }
 
 window.onload = loadCart;
